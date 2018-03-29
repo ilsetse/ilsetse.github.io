df51856a211357d9854c6a44bfc687f92f169c4a
ilsetse <ilsetse@gmail.com>
2018-03-29 19:32:44 +0300

    clean repo

--- assets/demos/portfolio_v1/js/masonry_grid.js
+++
@@ -1,7 +0,0 @@
-// external js: masonry.pkgd.js, imagesloaded.pkgd.js
-
-// init Isotope
-$('.grid').masonry({
-									 itemSelector: '.grid-item',
-									 isFitWidth: true // fill page width
-									 });
